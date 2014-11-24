9714583b2ea6844d076bda877612a5f902587c66
George Stagas <gstagas@gmail.com>
2014-11-24 02:13:43 +0200

    update tests

--- specs/cases/export-default.result.js
+++ specs/cases/export-default.result.js
@@ -1,25 +1,25 @@
 /* jshint esnext:true */
 var foo2;
-module.exports = 42;
-module.exports = function () {}
-function foo() {} module.exports = foo
-module.exports = (function(){function ____Class1(){"use strict";}return ____Class1;})()
-function Foo(){"use strict";} module.exports = Foo
-module.exports = {};
-module.exports = [];
-module.exports = foo2;
-var value2 = module.exports = 1;
+module.exports["default"] = 42;
+module.exports["default"] = function () {}
+function foo() {} module.exports["default"] = foo
+module.exports["default"] = (function(){function ____Class1(){"use strict";}return ____Class1;})()
+function Foo(){"use strict";} module.exports["default"] = Foo
+module.exports["default"] = {};
+module.exports["default"] = [];
+module.exports["default"] = foo2;
+var value2 = module.exports["default"] = 1;
 
 function A(){"use strict";}
   A.prototype.method=function() {"use strict";
     return 1;
   };
- module.exports = A
+ module.exports["default"] = A
 
 for(var B____Key in B){if(B.hasOwnProperty(B____Key)){A[B____Key]=B[B____Key];}}var ____SuperProtoOfB=B===null?null:B.prototype;A.prototype=Object.create(____SuperProtoOfB);A.prototype.constructor=A;A.__superConstructor__=B;function A(){"use strict";if(B!==null){B.apply(this,arguments);}}
   A.prototype.method=function() {"use strict";
     return 1;
   };
- module.exports = A
+ module.exports["default"] = A
 
 var x = 'should be on 13th line';
--- specs/cases/import-binding.result.js
+++ specs/cases/import-binding.result.js
@@ -1,4 +1,4 @@
-var mod = require("./mod.js");
+var mod = require("./mod.js")["default"];
 //import mod as name from "./mod.js";
 var name = require("./mod.js");
-var mod$0 = require("foo");var foo = mod$0;var bar = mod$0;
+var mod$0 = require("foo");var foo = mod$0["default"];var bar = mod$0;
